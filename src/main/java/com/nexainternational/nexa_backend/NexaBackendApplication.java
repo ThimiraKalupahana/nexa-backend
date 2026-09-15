b34f950a914eff3ac33d3957c96231e2a2ccd279
Thimira Deshaka <thimiradeshakashan0220@gmail.com>
2026-09-15 23:32:24 +0530

    Initialize project structure with Maven, Spring Boot, and essential configurations

---
+++ src/main/java/com/nexainternational/nexa_backend/NexaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexainternational.nexa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexainternational/nexa_backend/NexaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexainternational.nexa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
